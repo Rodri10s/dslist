9272fd24b7fe02193876fc2b6719414093b1826a
Ramon Rodrigues <ramonrodrigues838@gmail.com>
2025-01-22 21:07:37 -0300

    Relação muitos para muitos

--- dslist/src/main/java/com/ramon/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/ramon/dslist/controllers/GameController.java
@@ -3,14 +3,12 @@ package com.ramon.dslist.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramon.dslist.dto.GameMinDto;
-import com.ramon.dslist.entities.Game;
 import com.ramon.dslist.services.GameService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value = "/games")
